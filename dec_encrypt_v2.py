7a07ec62f3609396e832b0520de32fda35b15757
ZhukovOleksandr <77962262+ZhukovOleksandr@users.noreply.github.com>
2021-09-13 16:10:00 +0300

    Add decryption method
    Add decryption method  with "bad" input verification

--- dec_encrypt_v2.py
+++ dec_encrypt_v2.py
@@ -18,3 +18,26 @@ def encrypt(text, n):
         return encrypted[-1]
     else:
         return text
+
+
+def decrypt(encrypted, n):
+    """ Decrypts previously encrypted text using the sum of  'n' attribute
+    and the number required to complete the permutation cycle """
+    if encrypted and n > 0:
+        len_text = len(encrypted)
+        if len_text % 2 == 0:
+            enc_numb = len_text - n
+        else:
+            enc_numb = (len_text - n) - 1
+        result = encrypt(encrypted, enc_numb)
+        return result
+    else:
+        return encrypted
+
+
+if __name__ == '__main__':
+    my_str = 'Abcdefghij'
+    result_text = encrypt(my_str,3)
+    print(result_text)
+    print(decrypt(result_text,3))
+    print(decrypt(None, 3))

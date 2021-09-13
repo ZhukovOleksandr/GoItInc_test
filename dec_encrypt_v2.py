e849e28157b16c0af88c46681668ca64f50e2850
ZhukovOleksandr <77962262+ZhukovOleksandr@users.noreply.github.com>
2021-09-13 15:58:12 +0300

    Update dec_encrypt_v2.py

--- dec_encrypt_v2.py
+++ dec_encrypt_v2.py
@@ -1,26 +1,20 @@
 def encrypt(text, n):
-    even = []
-    odd = []
-    encrypted = [text]
-    iter_count = 0
+    """ Encrypts the given text using the 'n' attribute as number of iterations
+    of concatenation even and odd indexes in the string  """
+    if n > 0 and text:
+        even = []
+        odd = []
+        encrypted = [text]
+        iter_count = 0
 
-    while iter_count < n:
-        elem = encrypted[-1]
-        [even.append(elem[i]) for i in range(len(elem)) if i % 2 == 0]
-        [odd.append(elem[i]) for i in range(len(elem)) if i % 2 != 0]
-        encrypted.append(''.join(odd) + ''.join(even))
-        even.clear()
-        odd.clear()
-        iter_count += 1
-    return encrypted[-1]
-
-# def decrypt(encrypted, n):
-
-
-if __name__ == '__main__':
-    my_str = 'Abcdefghij'
-    a = encrypt(my_str, 10)
-    p = len(a)/2
-
-
-    # enc_text = 'bdfhjAcegi'
+        while iter_count < n:
+            elem = encrypted[-1]
+            [even.append(elem[i]) for i in range(len(elem)) if i % 2 == 0]
+            [odd.append(elem[i]) for i in range(len(elem)) if i % 2 != 0]
+            encrypted.append(''.join(odd) + ''.join(even))
+            even.clear()
+            odd.clear()
+            iter_count += 1
+        return encrypted[-1]
+    else:
+        return text
